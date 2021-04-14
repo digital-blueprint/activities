02b719322d5e1fa24ec80d424803346422244586
Patrizio Bekerle <patrizio@bekerle.com>
2021-04-14 10:01:40 +0200

    Add test skeleton

---
+++ packages/clipboard/test/unit.js
@@ -0,0 +1,26 @@
+// TODO: Implement
+
+// import {assert} from 'chai';
+
+import '../src/dbp-clipboard';
+
+suite('dbp-clipboard basics', () => {
+  // let node;
+
+  setup(async () => {
+    // node = document.createElement('dbp-clipboard');
+    // node.setAttribute('url', 'someurl');
+    // node.setAttribute('realm', 'somerealm');
+    // node.setAttribute('client-id', 'someId');
+    // document.body.appendChild(node);
+    // await node.updateComplete;
+  });
+
+  teardown(() => {
+    // node.remove();
+  });
+
+  test('should render', () => {
+    // assert.isNotNull(node.shadowRoot);
+  });
+});
