fce843983d0badccd77c6a140fc27de2fc239b17
Christoph Reiter <reiter.christoph@gmail.com>
2021-10-11 15:46:16 +0200

    Port to eslint 8

--- activity-showcase/src/dbp-clipboard-demo-activity.js
+++ activity-showcase/src/dbp-clipboard-demo-activity.js
@@ -4,7 +4,6 @@ import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
 import readme from '@dbp-topics/clipboard/README.md';
-import highlightCSSPath from 'highlight.js/styles/default.css';
 import * as demoStyles from "./styles";
 import {AdapterLitElement} from "@dbp-toolkit/provider/src/adapter-lit-element";
 import {DbpClipboardManagement} from "@dbp-topics/clipboard";
