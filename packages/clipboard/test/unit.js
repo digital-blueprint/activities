1d3c5242759b2bc61d5263489f929ca770920272
Christoph Reiter <reiter.christoph@gmail.com>
2022-02-01 15:58:46 +0100

    Reformat clipboard

--- packages/clipboard/test/unit.js
+++ packages/clipboard/test/unit.js
@@ -3,22 +3,22 @@ import {assert} from 'chai';
 import '../src/dbp-clipboard-management';
 
 suite('dbp-clipboard basics', () => {
-  let node;
+    let node;
 
-  setup(async () => {
-    node = document.createElement('dbp-clipboard-management');
-    node.setAttribute('url', 'someurl');
-    node.setAttribute('realm', 'somerealm');
-    node.setAttribute('client-id', 'someId');
-    document.body.appendChild(node);
-    await node.updateComplete;
-  });
+    setup(async () => {
+        node = document.createElement('dbp-clipboard-management');
+        node.setAttribute('url', 'someurl');
+        node.setAttribute('realm', 'somerealm');
+        node.setAttribute('client-id', 'someId');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
 
-  teardown(() => {
-    node.remove();
-  });
+    teardown(() => {
+        node.remove();
+    });
 
-  test('should render', () => {
-    assert.isNotNull(node.shadowRoot);
-  });
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
 });
