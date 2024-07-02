24d70147831820c9e5458e4fddfe8de12a08fe40
zsuffa david <zsuffa.david@gmail.com>
2024-07-02 11:38:04 +0200

    add group-manage package to the showcase

---
+++ activity-showcase/src/dbp-group-manage-demo-activity.js
@@ -0,0 +1,70 @@
+import {css, html} from 'lit';
+import {ScopedElementsMixin} from '@open-wc/scoped-elements';
+import {GroupManageDemo} from '../../packages/group-manage/src/dbp-group-manage-demo.js';
+import * as commonStyles from '@dbp-toolkit/common/styles';
+import * as commonUtils from "@dbp-toolkit/common/utils";
+// import {unsafeHTML} from 'lit/directives/unsafe-html.js';
+// import readme from '../../../toolkit/packages/group-manage/README.md';
+import * as demoStyles from './styles';
+import {AdapterLitElement} from '@dbp-toolkit/common';
+
+export class DbpGroupManagementDemoActivity extends ScopedElementsMixin(AdapterLitElement) {
+
+    constructor() {
+        super();
+        this.lang = 'en';
+        this.entryPointUrl = '';
+    }
+
+    static get scopedElements() {
+        return {
+            'dbp-group-manage-demo': GroupManageDemo,
+        };
+    }
+
+    static get properties() {
+        return {
+            ...super.properties,
+            lang: {type: String},
+            entryPointUrl: {type: String, attribute: 'entry-point-url'},
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        // this.updateComplete.then(() => {});
+    }
+
+    static get styles() {
+        // language=css
+        return [
+            commonStyles.getThemeCSS(),
+            commonStyles.getGeneralCSS(),
+            demoStyles.getDemoCSS(),
+            css`
+                h1.title {
+                    margin-bottom: 1em;
+                }
+                div.container {
+                    margin-bottom: 1.5em;
+                }
+
+                #demo {
+                    display: block;
+                    padding-top: 50px;
+                }
+            `,
+        ];
+    }
+
+    render() {
+        return html`
+            <dbp-group-manage-demo
+                id="group-manage-demo"
+                lang="${this.lang}"
+                entry-point-url="${this.entryPointUrl}"></dbp-group-manage-demo>
+        `;
+    }
+}
+
+commonUtils.defineCustomElement('dbp-group-manage-demo-activity', DbpGroupManagementDemoActivity);
